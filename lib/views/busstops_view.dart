7082de10fa1b223d86e8b89794253498aea83b56
lirc572 <lirc572@gmail.com>
2021-05-16 14:24:02 +0800

    feat: display liked bus stops in the order they were liked

--- lib/views/busstops_view.dart
+++ lib/views/busstops_view.dart
@@ -57,9 +57,16 @@ class _BusStopsViewState extends State<BusStopsView> {
         var busStops = await ApiProvider.fetchBusStops();
         var likedBusStops = c.liked;
         var busStopsReordered = [];
-        for (var busStop in busStops) {
-          if (likedBusStops.contains(busStop['caption'].toString())) {
-            busStopsReordered.add(busStop);
+        // for (var busStop in busStops) {
+        //   if (likedBusStops.contains(busStop['caption'].toString())) {
+        //     busStopsReordered.add(busStop);
+        //   }
+        // }
+        for (var likedBusStop in likedBusStops) {
+          for (var busStop in busStops) {
+            if (busStop['caption'].toString() == likedBusStop) {
+              busStopsReordered.add(busStop);
+            }
           }
         }
         for (var busStop in busStops) {
